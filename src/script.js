b2150070a52a76b21d86f45987f95a1de8f4056e
DennisGambini <dennis.gambini@gmail.com>
2022-04-07 15:08:41 +0200

    script: creato un template per costruire una card

--- src/script.js
+++ src/script.js
@@ -8,42 +8,62 @@
 // Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
 
 // definisco un array di oggetti
- const team = [
-     {
+const team = [
+    {
         nome: "Wayne Barnett", 
-        immagine: "img\wayne-barnett-founder-ceo.jpg",
+        immagine: "img/wayne-barnett-founder-ceo.jpg",
         ruolo: "Founder & CEO",
         
-     },
-     {
+    },
+    {
         nome: "Angela Caroll", 
-        immagine: "img\angela-caroll-chief-editor.jpg",
+        immagine: "img/angela-caroll-chief-editor.jpg",
         ruolo: "Chief Editor",
         
-     },
-     {
+    },
+    {
         nome: "Walter Gordon", 
-        immagine: "img\walter-gordon-office-manager.jpg",
+        immagine: "img/walter-gordon-office-manager.jpg",
         ruolo: "Office Manager",
         
-     },
-     {
+    },
+    {
         nome: "Angela Lopez", 
-        immagine: "img\angela-lopez-social-media-manager.jpg",
+        immagine: "img/angela-lopez-social-media-manager.jpg",
         ruolo: "Social Media Manager",
         
-     },
-     {
+    },
+    {
         nome: "Scott Estrada", 
-        immagine: "img\scott-estrada-developer.jpg",
+        immagine: "img/scott-estrada-developer.jpg",
         ruolo: "Developer",
         
-     },
-     {
+    },
+    {
         nome: "Barbara Ramos", 
-        immagine: "img\barbara-ramos-graphic-designer.jpg",
+        immagine: "img/barbara-ramos-graphic-designer.jpg",
         ruolo: "Graphic Designer",
         
-     }
- ]
- console.log(team)
+    }
+]
+console.log(team);
+
+const container = document.querySelector(".team-container");
+
+// creo template di una card
+let card = document.createElement("div");
+card.setAttribute("class", "team-card");
+card.innerHTML = `
+    <div class="card-image">
+        <img
+        src="${team[2].immagine}"
+        alt="${team[2].nome}"
+        />
+    </div>
+    <div class="card-text">
+        <h3>${team[2].nome}</h3>
+        <p>${team[2].ruolo}</p>
+    </div>
+`
+console.log(card)
+container.append(card);
